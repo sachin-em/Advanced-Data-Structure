1863fdb9b9f44a4cfe2e1af2bcc786d7dd9950ff
Sachin M <45335064+sachin-em@users.noreply.github.com>
2021-12-15 20:18:03 +0530

    Add files via upload

---
+++ Exercise 7 (Circular queue)/cqueue.c
@@ -0,0 +1,98 @@
+// Circular Queue implementation in C
+
+#include <stdio.h>
+
+#define SIZE 5
+
+int items[SIZE];
+int front = -1, rear = -1;
+
+// Check if the queue is full
+int isFull() {
+  if ((front == rear + 1) || (front == 0 && rear == SIZE - 1)) return 1;
+  return 0;
+}
+
+// Check if the queue is empty
+int isEmpty() {
+  if (front == -1) return 1;
+  return 0;
+}
+
+// Adding an element
+void enQueue(int element) {
+  if (isFull())
+    printf("\n Queue is full!! \n");
+  else {
+    if (front == -1) front = 0;
+    rear = (rear + 1) % SIZE;
+    items[rear] = element;
+    printf("\n Inserted -> %d", element);
+  }
+}
+
+// Removing an element
+int deQueue() {
+  int element;
+  if (isEmpty()) {
+    printf("\n Queue is empty !! \n");
+    return (-1);
+  } else {
+    element = items[front];
+    if (front == rear) {
+      front = -1;
+      rear = -1;
+    } 
+    // Q has only one element, so we reset the 
+    // queue after dequeing it. ?
+    else {
+      front = (front + 1) % SIZE;
+    }
+    printf("\n Deleted element -> %d \n", element);
+    return (element);
+  }
+}
+
+// Display the queue
+void display() {
+  int i;
+  if (isEmpty())
+    printf(" \n Empty Queue\n");
+  else {
+    printf("\n Front -> %d ", front);
+    printf("\n Items -> ");
+    for (i = front; i != rear; i = (i + 1) % SIZE) {
+      printf("%d ", items[i]);
+    }
+    printf("%d ", items[i]);
+    printf("\n Rear -> %d \n", rear);
+  }
+}
+
+int main() {
+  	int choice = 0, value;
+	
+	while(choice != 4){
+		printf("\n\n1.Enqueue\n2.Dequeue\n3.Display\n4.exit \nEnter ur choice : ");
+		scanf("%d", &choice);
+		switch(choice){
+			case 1:
+                printf("\nEnter data to be inserted : ");
+                scanf("%d",&value);
+				enQueue(value);
+				break;
+			case 2:
+				deQueue();
+				break;
+			case 3:
+				display();
+				break;
+			case 4:
+				break;
+			default:
+				printf("Invalid choice...!");
+		}
+	}
+
+  return 0;
+}
